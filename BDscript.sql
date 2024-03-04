9d96e5f1fb152871a5026c5c68ba106078e92c6c
apotanin <apotanin@simplex48.ru>
2024-03-04 16:22:56 +0300

    +скрипт БД

---
+++ BDscript.sql
@@ -0,0 +1,28 @@
+---------------------------------------------------------------------------------------- Скрипт создания
+CREATE TABLE PRODUCTS(
+PROD_ID INT PRIMARY KEY IDENTITY, 
+PROD_NAME VARCHAR(100) NOT NULL);
+
+CREATE TABLE CATEGORY(
+CATEG_ID INT PRIMARY KEY IDENTITY,
+CATEG_NAME VARCHAR(100) NOT NULL);
+
+CREATE TABLE PRODUCT_CATEGORY(
+F_PROD_ID INT NOT NULL,
+F_CATEG_ID INT NOT NULL,
+FOREIGN KEY(F_PROD_ID) REFERENCES Products(PROD_ID) ON DELETE CASCADE,
+FOREIGN KEY(F_CATEG_ID) REFERENCES Category(CATEG_ID) ON DELETE CASCADE);
+
+CREATE UNIQUE INDEX IND_PRODUCT_CATEGORY ON PRODUCT_CATEGORY(F_PROD_ID, F_CATEG_ID);
+---------------------------------------------------------------------------------------- Наполение
+
+INSERT INTO PRODUCTS VALUES('Лук'), ('Нож'), ('Кастрюля');
+INSERT INTO CATEGORY VALUES('Оружие'), ('Кухонные приборы');
+INSERT INTO PRODUCT_CATEGORY VALUES(1, 1), (2, 1), (2, 2);
+---------------------------------------------------------------------------------------- Вывод
+
+SELECT P.PROD_NAME AS 'Продукт', C.CATEG_NAME AS 'Категория'
+FROM PRODUCTS P
+LEFT JOIN PRODUCT_CATEGORY P_C ON P.PROD_ID = P_C.F_PROD_ID
+LEFT JOIN CATEGORY C ON C.CATEG_ID = P_C.F_CATEG_ID
+ORDER BY P.PROD_NAME;
